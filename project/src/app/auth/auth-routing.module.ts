3db32cafd8973a188fc132f346deeb76b216aec4
Joao Matheus Guedes da Costa <joao.guedes.da.costa@avanade.com>
2019-06-07 12:52:02 -0300

    Primeiro commit

---
+++ project/src/app/auth/auth-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule, Component } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [{
+  path: ''
+  Component:
+}];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class AuthRoutingModule { }
