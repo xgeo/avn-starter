0c5eeda645e97ecaf744d8293d09517719b25e64
josecarlosmonteiro <1monteirocarlos@gmail.com>
2019-06-07 12:52:56 -0300

    removendo alterações no arquivo de rota

--- project/src/app/app-routing.module.ts
+++ project/src/app/app-routing.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [{
-  path: '',
-  component: 
-}];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
